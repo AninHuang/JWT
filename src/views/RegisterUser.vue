c0aff0933913f44769fbd43312b0f9a709cbe3fe
AninHuang <lifeanne19904@gmail.com>
2020-03-30 21:15:07 +0800

    handling errors

--- src/views/RegisterUser.vue
+++ src/views/RegisterUser.vue
@@ -11,6 +11,11 @@
       <input v-model="password" type="password" name="password" value />
 
       <button type="submit" name="button">Register</button>
+
+      <ul>
+        <li v-for="(error, index) in errors" :key="index">{{ error }}</li>
+      </ul>
+
       <router-link to="/login">Already have an account? Login.</router-link>
     </form>
   </div>
@@ -22,7 +27,8 @@ export default {
     return {
       name: '',
       email: '',
-      password: ''
+      password: '',
+      errors: null
     }
   },
   methods: {
@@ -35,6 +41,9 @@ export default {
         })
         .then(() => {
           this.$router.push({ name: 'dashboard' })
+        })
+        .catch(err => {
+          this.errors = err.response.data.error
         })
     }
   }
